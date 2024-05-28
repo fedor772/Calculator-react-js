8cd7129236922efd6b492b0041fc91a36063b47b
fedor772 <160321057+fedor772@users.noreply.github.com>
2024-05-28 18:58:30 +0000

    Deloy v2

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,4 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  }
-})
+export default defineConfig({ base: "/{your-repo-name}", plugins: [react()], })
